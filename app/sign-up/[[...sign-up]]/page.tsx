2b74db60c2cf7e840285326773018239ad83f502
Vivek Ghode <70033589+VivekGhode27@users.noreply.github.com>
2026-09-08 17:05:46 +0530

    Create page.tsx

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignUp } from "@clerk/nextjs";
+export default function SignUpPage() {
+  return (
+    <div style={{ minHeight:'100vh', display:'flex', alignItems:'center', justifyContent:'center', background:'#F8F8F6' }}>
+      <SignUp />
+    </div>
+  );
+}
