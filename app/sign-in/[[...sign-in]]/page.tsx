983a34a5015bc01bec0894646d9e2c32bd8e995a
Vivek Ghode <70033589+VivekGhode27@users.noreply.github.com>
2026-09-08 17:03:31 +0530

    Create page.tsx

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignIn } from "@clerk/nextjs";
+export default function SignInPage() {
+  return (
+    <div style={{ minHeight:'100vh', display:'flex', alignItems:'center', justifyContent:'center', background:'#F8F8F6' }}>
+      <SignIn />
+    </div>
+  );
+}
